cc49a0e54d187f1239124d10bbb3b84592ca4f1a
chi828 <124188695+chi828@users.noreply.github.com>
2023-11-17 14:37:17 +0100

    Initial Commit

---
+++ SeriouslyHow/SeriouslyHowApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeriouslyHowApp.swift
+//  SeriouslyHow
+//
+//  Created by Chiara Giorgia Ricci on 17/11/23.
+//
+
+import SwiftUI
+
+@main
+struct SeriouslyHowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
